9a9315565dc7f831ba6815ed62b6d7773630df28
SanderVdB <mr.rednas@gmail.com>
2015-05-25 14:50:15 +0200

    removed databasemanager

--- Finale_opdracht/src/main/java/util/Databasemanager.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public abstract class Databasemanager {
-
-	public abstract boolean databaseExists();
-
-	public abstract boolean createDatabase(String url, String username, String password);
-}
